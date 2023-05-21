557faed90e446b86b9db149b39139b2296301028
Paulo Felipe <paulofeliperezende@outlook.com>
2023-05-20 23:58:21 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,10 @@
+const audio = document.getElementById('audio');
+const links = document.querySelectorAll('#playlist a');
+
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    audio.src = this.dataset.src;
+    audio.play();
+  });
+});
