da4e499e7f80536040550ddbd1458486c18696bf
Paulo Felipe <99059267+paulofelipere@users.noreply.github.com>
2023-05-21 21:23:59 -0300

    Update musicalbox.js

--- musicalbox.js
+++ musicalbox.js
@@ -4,6 +4,6 @@ links.forEach(links => {
   links.addEventListener('click', function(e) {
     e.preventDefault();
     audio.src = this.dataset.src;
-    audio.play();
+    audio.play(links);
   });
 });
