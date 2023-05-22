db24daa6e486f831f83074457f5c5aee1a63c788
Paulo Felipe <99059267+paulofelipere@users.noreply.github.com>
2023-05-21 21:17:33 -0300

    Update musicalbox.js

--- musicalbox.js
+++ musicalbox.js
@@ -1,7 +1,7 @@
 const links = document.querySelectorAll('#playlist a');
 
-links.forEach(link => {
-  link.addEventListener('click', function(e) {
+links.forEach(links => {
+  links.addEventListener('click', function(e) {
     e.preventDefault();
     audio.src = this.dataset.src;
     audio.play();
