a260204501013bd46470cc31d3c8148231002da4
Paulo Felipe <99059267+paulofelipere@users.noreply.github.com>
2023-05-22 00:38:45 -0300

    Update musicalbox.js

--- musicalbox.js
+++ musicalbox.js
@@ -1,12 +1,12 @@
-function setupPlaylist(links) {
-  links.forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      audio.src = this.dataset.src;
-      audio.play();
-    });
-  });
-}
+const doc = document;
+const playlist = doc.querySelector('#playlist');
+const links = playlist.querySelectorAll('a');
+const audio = doc.querySelector('audio');
 
-const playlistLinks = Array.from(document.querySelectorAll('#playlist a'));
-setupPlaylist(playlistLinks);
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    audio.src = this.dataset.src;
+    audio.play();
+  });
+});
